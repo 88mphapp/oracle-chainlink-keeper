b5ec8f154bfd6a11a88af7c4113af3106d977f54
Zefram Lou <zeframlou@gmail.com>
2021-04-13 18:05:48 -0700

    Add kovan network and deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,7 @@ require('hardhat-deploy')
 require('hardhat-gas-reporter')
 require('@nomiclabs/hardhat-solhint')
 require('hardhat-spdx-license-identifier')
+require('@nomiclabs/hardhat-etherscan')
 
 let secret
 
@@ -49,6 +50,16 @@ module.exports = {
         url: 'https://eth-mainnet.alchemyapi.io/v2/pvGDp1uf8J7QZ7MXpLhYs_SnMnsE0TY5',
         blockNumber: 12200000
       }
+    },
+    kovan: {
+      url: 'https://eth-kovan.alchemyapi.io/v2/Bm0nm12ihwQoatvltj8hQyvZxFDePVTN',
+      chainId: 0x2a,
+      from: secret.account,
+      accounts: {
+        mnemonic: secret.mnemonic
+      },
+      gas: 'auto',
+      gasPrice: 1e9
     }
   },
   spdxLicenseIdentifier: {
@@ -56,5 +67,8 @@ module.exports = {
   },
   mocha: {
     timeout: 120000
+  },
+  etherscan: {
+    apiKey: 'SCTNNP3MJK18WV84QIX6WPGMWIS8H1J9W7'
   }
 }
